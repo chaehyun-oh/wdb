9218f53a9b2aa2be549ab9eb47b8c2960bc2d807
chaehyun_oh <ohchaehh@gmail.com>
2023-08-31 23:26:56 +0900

    230831 update

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -5,41 +5,48 @@ for (var i = 0; i < drumBtn.length; i++) {
 
         var buttonInnerHTML = this.innerHTML;
 
-        switch (buttonInnerHTML) {
-            case "w":
-                var sound = new Audio('./sounds/tom-1.mp3');
-                sound.play();
-                break;
-            case "a":
-                var sound = new Audio('./sounds/tom-2.mp3');
-                sound.play();
-                break;
-            case "s":
-                var sound = new Audio('./sounds/tom-3.mp3');
-                sound.play();
-                break;
-            case "d":
-                var sound = new Audio('./sounds/tom-4.mp3');
-                sound.play();
-                break;
-
-            case "j":
-                var sound = new Audio('./sounds/snare.mp3');
-                sound.play();
-                break;
-            case "k":
-                var sound = new Audio('./sounds/crash.mp3');
-                sound.play();
-                break;
-            case "l":
-                var sound = new Audio('./sounds/kick-bass.mp3');
-                sound.play();
-                break;
-            default:
-                console.log(buttonInnerHTML);
-                break;
-        }
-
-    })
+        makeSound(buttonInnerHTML);
+    });
 }
 
+document.addEventListener('keypress', function (e) {
+    makeSound(e.key);
+})
+
+function makeSound(key) {
+
+    switch (key) {
+        case "w":
+            var sound = new Audio('./sounds/tom-1.mp3');
+            sound.play();
+            break;
+        case "a":
+            var sound = new Audio('./sounds/tom-2.mp3');
+            sound.play();
+            break;
+        case "s":
+            var sound = new Audio('./sounds/tom-3.mp3');
+            sound.play();
+            break;
+        case "d":
+            var sound = new Audio('./sounds/tom-4.mp3');
+            sound.play();
+            break;
+
+        case "j":
+            var sound = new Audio('./sounds/snare.mp3');
+            sound.play();
+            break;
+        case "k":
+            var sound = new Audio('./sounds/crash.mp3');
+            sound.play();
+            break;
+        case "l":
+            var sound = new Audio('./sounds/kick-bass.mp3');
+            sound.play();
+            break;
+        default:
+            console.log(buttonInnerHTML);
+            break;
+    }
+}
--- README.md
+++ README.md
@@ -8,3 +8,4 @@
 | [Day 1]() | S.18 Advanced JavaScript and DOM Manipulation (135~137) |
 | [Day 2]() | S.18 Advanced JavaScript and DOM Manipulation (138~139) |
 | [Day 3]() | S.18 Advanced JavaScript and DOM Manipulation (140~142) |
+| [Day 4]() | S.18 Advanced JavaScript and DOM Manipulation (143~145) |
